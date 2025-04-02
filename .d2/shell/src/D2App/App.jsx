da772475cb56edcbb32832fb80444c45715ffdcd
bed-com-23-20 <bed-com-23-20@unima.ac.mw>
2025-04-02 17:20:11 +0200

    merging

---
+++ .d2/shell/src/D2App/App.jsx
@@ -0,0 +1,35 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import React from 'react'
+import classes from './App.module.css'
+import Enrollment from './componets/Enrollment'
+import EnrollmentForm from './componets/EnrollmentForm'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+const query = {
+    me: {
+        resource: 'me',
+    },
+}
+
+const MyApp = () => {
+    const { error, loading, data } = useDataQuery(query)
+
+    if (error) {
+        return <span>{i18n.t('ERROR')}</span>
+    }
+
+    if (loading) {
+        return <span>{i18n.t('Loading...')}</span>
+    }
+
+    return (
+        <div className={classes.container}>
+            <Enrollment />
+            {/* <EnrollmentForm /> */}
+            
+        </div>
+    )
+}
+
+export default MyApp
